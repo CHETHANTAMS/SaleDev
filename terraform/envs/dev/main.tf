1f73c33b0082e29f2cd4f9edbb793a29a24b0d3e
cs-test1 <schethan112@gmail.com>
2026-01-20 21:04:08 +0530

    delete bin

--- terraform/envs/dev/main.tf
+++
@@ -1,8 +0,0 @@
-module "ec2" {
-  source = "../../modules/ec2"
-
-  ami_id        = var.ami_id
-  instance_type = var.instance_type
-  instance_name = "dev-ec2"
-}
-
--- terraform/envs/dev/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-aws_region     = "us-west-1"
-ami_id         = "ami-0e6a50b0059fd2cc3" # replace with valid AMI
-instance_type = "t2.micro"
-
